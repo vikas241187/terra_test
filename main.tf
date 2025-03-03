9d706a0d3d2df391a9a33412d73cac4d31487502
vikas241187 <vikas.foxy@gmail.com>
2025-03-03 22:50:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,6 @@ Name = "Git-instance"
 }
 resource "aws_ebs_volume" "git-vol" {
 size = var.env == "prod" ? 8 : 4
-aws_insatance_id = aws_instance.git_instance.id
-volume_id = aws_ebs_volume.git-vol.id
 availability_zone = var.az
 tags ={
 Name = "git-vol"
